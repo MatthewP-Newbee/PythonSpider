cc5428fb3927e58bbae51659f8c5098c53c4a0bf
MatthewP-Newbee <matthewhakka@gmail.com>
2019-01-20 00:22:06 +0800

    Update

---
+++ Scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# @Project: PythonSpider
+# @Date: 2019/1/19
+# @Author: MatthewP
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    # Spider id. 同一个爬虫项目必须使用同一个id。同时不能与其他项目重名。
+    name = 'quotes'
+
+    # start_requests方法必须返回可迭代的Requests对象。
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split('/')[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
